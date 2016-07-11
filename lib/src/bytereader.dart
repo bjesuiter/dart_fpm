f0c7536d0fc3193f5f57dd28600e8d64d9ed8299
Nicky Gruner <nickygruner@gmail.com>
2016-07-11 15:19:43 +0200

    implemented comfortable ByteReader class

---
+++ lib/src/bytereader.dart
@@ -0,0 +1,24 @@
+library dart_fpm.byte_reader;
+
+import 'dart:typed_data';
+
+class ByteReader {
+
+  final Uint8List _data;
+  int _offset;
+
+  ByteReader (List<int>data, {offset : 0}) :
+        _data = new Uint8List.fromList(data),
+        _offset = offset;
+
+  int get nextByte => _data.elementAt(_offset++);
+
+  int get nextShort => (nextByte << 8) | nextByte;
+
+  int get nextInt => (nextShort << 16) | nextShort;
+
+  int get nextVarByte => ((_data.elementAt(_offset) >> 7) == 0) ? nextByte : nextInt;
+
+  bool available (int bytes) => (_data.lengthInBytes - _offset) >= bytes;
+
+}

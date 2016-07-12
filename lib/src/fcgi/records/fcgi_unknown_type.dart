ba4cc8c199688a2115f3c6b308a2f6584d286c2a
Nicky Gruner <nickygruner@gmail.com>
2016-07-12 12:04:32 +0200

    implemented FcgiUnknownRecordBody class

---
+++ lib/src/fcgi/records/fcgi_unknown_type.dart
@@ -0,0 +1,18 @@
+library dart_fpm.fcgi.records.unknown_type_body;
+
+import 'package:dart_fpm/src/fcgi/fcgi.dart';
+import 'package:dart_fpm/src/bytewriter.dart';
+
+class FcgiUnknownTypeBody extends FcgiRecordBody {
+
+  final int unknownType;
+
+  FcgiUnknownTypeBody(this.unknownType);
+
+  FcgiRecordType get type => FcgiRecordType.UNKNOWN_TYPE;
+
+  @override
+  List<int> toByteStream() => new ByteWriter().addByte(unknownType).addSpace(7)
+      .takeBytes();
+
+}
--- lib/src/fcgi/records/records.dart
+++ lib/src/fcgi/records/records.dart
@@ -5,3 +5,4 @@ export 'fcgi_record_header.dart';
 export 'fcgi_record_body.dart';
 export 'fcgi_begin_request.dart';
 export 'fcgi_end_request.dart';
+export 'fcgi_unknown_type.dart';

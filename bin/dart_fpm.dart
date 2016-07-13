a4b0d3da3b124354e8af4e8eb9a99617b36d052e
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-13 16:53:28 +0200

    updated main file

--- bin/dart_fpm.dart
+++ bin/dart_fpm.dart
@@ -1,2 +1,42 @@
 library dart_fpm.bin;
 
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:logging/logging.dart';
+import 'package:dart_fpm/dart_fpm.dart';
+
+/// maps request ids to detailed request information
+Map<int, dynamic> requests;
+
+Logger _libLogger = new Logger("dart_fpm");
+
+main() async {
+  hierarchicalLoggingEnabled = true;
+
+  ServerSocket serverSocket = await ServerSocket.bind(InternetAddress.LOOPBACK_IP_V4, 9999);
+
+  await for (var socket in serverSocket) {
+    socket.write("Socket connected. Hello Dude!\n");
+
+    socket
+        .transform(new FcgiRecordTransformer())
+        .listen(
+        (FcgiRecord record) {
+      //TODO: implement record handling
+      _libLogger.info(record);
+    }, onError: (data) {
+      if (data is FcgiRecord)
+        socket.add(data.toByteStream());
+    });
+
+//    socket.listen((data) {
+//      contentBuilder.add(data);
+//      counter++;
+//      if (counter > 0) {
+//        var buffer = new Uint8List.fromList(contentBuilder.takeBytes());
+//        socket.write("${buffer.take(8)}\n");
+//        counter = 0;
+//      }
+//    });
+  }
+}

3a0cf3cb147f02c2a71b0176e9d6a6938b0e8f73
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-14 00:31:02 +0200

    implemented first version with output to browser, crashes during processing

--- bin/dart_fpm.dart
+++ bin/dart_fpm.dart
@@ -2,6 +2,7 @@ library dart_fpm.bin;
 
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:convert';
 import 'package:logging/logging.dart';
 import 'package:logging_handlers/server_logging_handlers.dart';
 import 'package:dart_fpm/dart_fpm.dart';
@@ -20,18 +21,70 @@ main() async {
 
   ServerSocket serverSocket = await ServerSocket.bind(InternetAddress.LOOPBACK_IP_V4, 9999);
 
+  //only for testing!!!
+  int counter = 0;
+
   await for (var socket in serverSocket) {
     socket
         .transform(new FcgiRecordTransformer())
-        .listen(
-        (FcgiRecord record) {
+        .listen((FcgiRecord record) {
       //TODO: implement record handling
-      _libLogger.info(record);
+      _libLogger.info(record.header.type.toString());
+
+      FcgiRecord response;
+
+      //only for testing
+      counter++;
+//      if (counter < 4) return;
+
+      //IMPORTANT: SEND CONTENT TYPE OF RETURN FIRST!!!
+      response = new FcgiRecord.generateResponse(record.header.requestId,
+          new FcgiStreamBody(FcgiRecordType.STDOUT, new AsciiCodec().encode("Content-type: text/html\r\n\r\n")));
+
+      socket.add(response.toByteStream());
+
+      response = new FcgiRecord.generateResponse(record.header.requestId,
+          new FcgiStreamBody(FcgiRecordType.STDOUT, new AsciiCodec().encode("Hello World!")));
+
+      socket.add(response.toByteStream());
+
+//        response = new FcgiRecord.generateResponse(record.header.requestId,
+//            new FcgiStreamBody(FcgiRecordType.DATA, new AsciiCodec().encode(
+//                '''
+//                <!DOCTYPE html>
+//<html>
+//<body>
+//
+//<h1>My First Heading</h1>
+//
+//<p>My first paragraph.</p>
+//
+//</body>
+//</html>
+//
+//
+//                '''
+//            )));
+//
+//        socket.add(response.toByteStream());
+
+      socket.flush().then((data) {
+        response = new FcgiRecord.generateResponse(record.header.requestId,
+            new FcgiEndRequestBody(0, FcgiProtocolStatus.REQUEST_COMPLETE));
+
+        socket.add(response.toByteStream());
+
+        counter = 0;
+      });
     }, onError: (data) {
+      //TODO: check if stream is already closed (SocketException)
+      if (data is SocketException) {
+        //clean all available things for this requestID
+        return;
+      }
+
       if (data is FcgiRecord)
         socket.add(data.toByteStream());
-    }, onDone: () {
-      //TODO: catch closed socket connection errors
     });
 
 //    socket.listen((data) {

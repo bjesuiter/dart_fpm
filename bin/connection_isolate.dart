b200f08156ccc16df8f866e7e11763725ba1ed0c
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-18 11:26:43 +0200

    added implementation of connection isolate

--- bin/connection_isolate.dart
+++ bin/connection_isolate.dart
@@ -1,13 +1,17 @@
 library dart_fpmt.connection_isolate;
 
 import 'dart:isolate';
-import 'dart:async';
-import 'package:isolate/ports.dart';
+import 'dart:io';
+import 'handle_connection.dart';
 import 'package:stream_channel/stream_channel.dart';
 
 void main (List<String> args, SendPort reply) {
   var channel = new IsolateChannel.connectSend(reply);
 
-
+  channel.stream.listen( (event) {
+    if (event is Socket) {
+      handleConnection(event);
+    }
+  });
 
 }

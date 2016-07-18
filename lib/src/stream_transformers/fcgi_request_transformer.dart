7ea3cea525b00d3a5e07cbca8418c4aa06ae87ef
Nicky Gruner <nickygruner@gmail.com>
2016-07-18 12:15:59 +0200

    implemented request transformer

--- lib/src/stream_transformers/fcgi_request_transformer.dart
+++ lib/src/stream_transformers/fcgi_request_transformer.dart
@@ -3,10 +3,50 @@ library dart_fpm.fcgi_request_transformer;
 import 'dart:async';
 import '../fcgi/fcgi.dart';
 
-class FcgiRequestTransformer implements StreamTransformer<FcgiRecord, dynamic> {
+class FcgiRequestTransformer implements StreamTransformer<FcgiRecord, Request> {
+
+  final StreamController<Request> _streamController = new StreamController();
+  final StreamSink<FcgiRecord> managementRecords;
+
+  final Map<int, Request> _requests = new Map();
+
+  /// stream sink to handle management records
+  FcgiRequestTransformer(this.managementRecords);
 
   @override
-  Stream bind(Stream<FcgiRecord> stream) {
-    // TODO: implement bind
+  Stream<Request> bind(Stream<FcgiRecord> stream) {
+    stream.listen((record) {
+      _handleRecord(record);
+    });
+    return _streamController.stream;
+  }
+
+  void _handleRecord (FcgiRecord record) {
+    if (record.header.requestId == FCGI_NULL_REQUEST_ID) {
+      managementRecords.add(record);
+      return;
+    }
+    if (!_requests.containsKey(record.header.requestId) && record.header.type != RecordType.BEGIN_REQUEST) {
+      _streamController.addError(new RangeError.index(record.header.requestId, _requests.keys));
+      return;
+    }
+    if (record.header.type == RecordType.STDIN && record.body.contentLength == 0) {
+      _streamController.add(_requests.remove(record.header.requestId));
+      return;
+    }
+    switch (record.header.type) {
+      case RecordType.BEGIN_REQUEST:
+        _requests[record.header.requestId] = new Request(record);
+        break;
+      case RecordType.PARAMS:
+        _requests[record.header.requestId].addParams(record.body);
+        break;
+      case RecordType.STDIN:
+      case RecordType.DATA:
+        _requests[record.header.requestId].addData(record.body);
+        break;
+      default:
+        _streamController.addError(new RangeError("invalid record type"));
+    }
   }
 }

62b39ab2139d9433e02372e7f1e2ff5c2b7d85ba
Nicky Gruner <nickygruner@gmail.com>
2016-07-18 13:39:39 +0200

    implemented response class

--- lib/src/fcgi/requests/response.dart
+++ lib/src/fcgi/requests/response.dart
@@ -3,12 +3,16 @@ library dart_fpm.fcgi.requests.response;
 import 'package:dart_fpm/src/fcgi/fcgi.dart';
 import 'dart:async';
 
+typedef void DataFunction(Response, String);
+typedef void DoneFunction(Response);
+
 class Response {
 
   final int requestId;
   ProtocolStatus protocolStatus = ProtocolStatus.REQUEST_COMPLETE;
   int appStatus = 0;
-  final Map<String, String> header = new Map();
+  final List<String> _header = new List();
+  bool _headerSent = false;
   final StreamController<String> _output = new StreamController();
 
   Response._(this.requestId);
@@ -16,15 +20,34 @@ class Response {
   factory Response (int requestId, Function onData, Function onError,
       Function onDone) {
     Response response = new Response._(requestId);
-    response._output.stream.listen(onData, onError: onError, onDone: onDone,
-        cancelOnError: false);
+    response._createListener(onData, onError, onDone);
     return response;
+  }
+
+  void _createListener(DataFunction onData, DataFunction onError,
+      DoneFunction onDone) {
+    _output.stream.listen((data) {
+      if (!_headerSent) {
+        onData(this, "${_header.join("\r\n")}\r\n\r\n");
+        _headerSent;
+      }
+      onData(this, data);
+    }, onError: (error) {
+      onError(this, error);
+    }, onDone: () {
+      onDone(this);
+    },
+        cancelOnError: false);
   }
 
   StreamSink<String> get output  => _output;
 
   void close () {
     _output.close();
+  }
+
+  void header(dynamic entry) {
+    _header.add(entry.toString());
   }
 
 }

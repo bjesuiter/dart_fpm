40bffd9f0a2af83b54d0c81ce9a4f3328d7f358e
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-18 10:10:58 +0200

    renamed fcgi request transformer

--- lib/src/stream_transformers/fcgi_request_transformer.dart
+++ lib/src/stream_transformers/fcgi_request_transformer.dart
@@ -3,7 +3,7 @@ library dart_fpm.fcgi_request_transformer;
 import 'dart:async';
 import '../fcgi/fcgi.dart';
 
-class RequestTransformer implements StreamTransformer<FcgiRecord, dynamic> {
+class FcgiRequestTransformer implements StreamTransformer<FcgiRecord, dynamic> {
 
   @override
   Stream bind(Stream<FcgiRecord> stream) {

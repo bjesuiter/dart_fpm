7521c261081119d702b6e3cb9f5392bd35b54a70
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-19 14:17:07 +0200

    added stdout and stderr SendPorts

--- lib/src/fcgi/requests/response.dart
+++ lib/src/fcgi/requests/response.dart
@@ -1,6 +1,8 @@
 library dart_fpm.fcgi.requests.response;
 
 import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
 
 import 'package:dart_fpm/src/fcgi/fcgi.dart';
 
@@ -15,9 +17,16 @@ class Response implements StreamSink {
 
   final Request request;
 
-  int get requestId => request.requestId;
+  final ReceivePort _stdout;
+
+  SendPort get stdout => _stdout.sendPort;
+
+  final ReceivePort _stderr;
+
+  SendPort get stderr => _stderr.sendPort;
 
   ProtocolStatus protocolStatus = ProtocolStatus.REQUEST_COMPLETE;
+
   int appStatus = 0;
   final List<String> _header = new List();
   bool _headerSent = false;
@@ -31,32 +40,14 @@ class Response implements StreamSink {
 
   Response._(this.request);
 
+  @override
+  Future get done => _output.done;
+
   FcgiRecord get endRequestRecord =>
       new FcgiRecord.generateResponse(requestId, new EndRequestBody(appStatus, protocolStatus));
 
-  StreamSink<String> get output => _output;
+  int get requestId => request.requestId;
 
-  Future close() =>
-      _output.close();
-
-
-  void header(dynamic entry) {
-    _header.add(entry.toString());
-  }
-
-  void _createListener(DataFunction onData, DataFunction onError, DoneFunction onDone) {
-    _output.stream.listen((data) {
-      if (!_headerSent) {
-        onData(this, "${_header.join("\r\n")}\r\n\r\n");
-        _headerSent;
-      }
-      onData(this, data);
-    }, onError: (error) {
-      onError(this, error);
-    }, onDone: () {
-      onDone(this);
-    }, cancelOnError: false);
-  }
 
   @override
   void add(event) {
@@ -71,7 +62,25 @@ class Response implements StreamSink {
   @override
   Future addStream(Stream stream) => _output.addStream(stream);
 
+  Future close() =>
+      _output.close();
 
-  @override
-  Future get done => _output.done;
+  void header(dynamic entry) {
+    _header.add(entry.toString());
+  }
+
+
+  void _createListener(DataFunction onData, DataFunction onError, DoneFunction onDone) {
+    _output.stream.listen((data) {
+      if (!_headerSent) {
+        onData(this, "${_header.join("\r\n")}\r\n\r\n");
+        _headerSent;
+      }
+      onData(this, data);
+    }, onError: (error) {
+      onError(this, error);
+    }, onDone: () {
+      onDone(this);
+    }, cancelOnError: false);
+  }
 }

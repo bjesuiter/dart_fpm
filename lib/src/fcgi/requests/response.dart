42a9a2a4ea726886a9f311e49bb9e252f8d69b9a
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-19 13:57:39 +0200

    implemented StreamSink

--- lib/src/fcgi/requests/response.dart
+++ lib/src/fcgi/requests/response.dart
@@ -8,35 +8,37 @@ typedef void DataFunction(Response, String);
 
 typedef void DoneFunction(Response);
 
-class Response {
+class Response implements StreamSink {
 
   //TODO: move isolate to response object
   //TODO: move control / data / error ports into response object from connection_handler
 
+  final Request request;
 
-  final int requestId;
+  int get requestId => request.requestId;
+
   ProtocolStatus protocolStatus = ProtocolStatus.REQUEST_COMPLETE;
   int appStatus = 0;
   final List<String> _header = new List();
   bool _headerSent = false;
   final StreamController<String> _output = new StreamController();
 
-  factory Response(int requestId, Function onData, Function onError, Function onDone) {
-    Response response = new Response._(requestId);
+  factory Response(Request request, Function onData, Function onError, Function onDone) {
+    Response response = new Response._(request);
     response._createListener(onData, onError, onDone);
     return response;
   }
 
-  Response._(this.requestId);
+  Response._(this.request);
 
   FcgiRecord get endRequestRecord =>
       new FcgiRecord.generateResponse(requestId, new EndRequestBody(appStatus, protocolStatus));
 
   StreamSink<String> get output => _output;
 
-  void close() {
-    _output.close();
-  }
+  Future close() =>
+      _output.close();
+
 
   void header(dynamic entry) {
     _header.add(entry.toString());
@@ -55,4 +57,21 @@ class Response {
       onDone(this);
     }, cancelOnError: false);
   }
+
+  @override
+  void add(event) {
+    _output.add(event);
+  }
+
+  @override
+  void addError(errorEvent, [StackTrace stackTrace]) {
+    _output.addError(errorEvent, stackTrace);
+  }
+
+  @override
+  Future addStream(Stream stream) => _output.addStream(stream);
+
+
+  @override
+  Future get done => _output.done;
 }

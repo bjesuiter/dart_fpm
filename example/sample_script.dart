1a194bfad592f165f3d5ae6ec4b45e0d5f8f1ef2
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-09-07 21:37:51 +0200

    added param and stdin values

--- example/sample_script.dart
+++ example/sample_script.dart
@@ -1,16 +1,15 @@
-
-import 'dart:async';
+import 'dart:convert';
 import 'dart:isolate';
 import 'dart:io';
 
-main (args, SendPort stdout) {
-
+main(List<String> args, SendPort stdout) {
   //Output content through stdout
   stdout.send("This is a test Message from SampleScript <br>");
 
-  //Access Parameters through environment variables
-  Map<String, String> env = Platform.environment;
-  env.forEach((k, v) => stdout.send("Key=$k Value=$v <br>"));
+  var stdin = args[0];
+  Map<String, String> params = JSON.decode(args[1]);
+
+  params.forEach((k, v) => stdout.send("Key=$k Value=$v <br>"));
 
   //Return exitCode different from 0 for testing
   return 2;

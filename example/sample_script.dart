307475a1f3c1fc5cc66962c6922def4554dd0c06
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-09-07 20:45:50 +0200

    added param print

--- example/sample_script.dart
+++ example/sample_script.dart
@@ -1,10 +1,16 @@
 
 import 'dart:async';
 import 'dart:isolate';
+import 'dart:io';
 
 main (args, SendPort stdout) {
 
-  stdout.send("This is a test Message from SampleScript");
+  //Output content through stdout
+  stdout.send("This is a test Message from SampleScript <br>");
+
+  //Access Parameters through environment variables
+  Map<String, String> env = Platform.environment;
+  env.forEach((k, v) => stdout.send("Key=$k Value=$v <br>"));
 
   //Return exitCode different from 0 for testing
   return 2;

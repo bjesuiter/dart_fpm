49e79619aa64f6d2ebadcf458616382a2fc87db4
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-09-07 19:42:21 +0200

    added a test output

--- example/sample_script.dart
+++ example/sample_script.dart
@@ -1,4 +1,11 @@
 
-main () {
+import 'dart:async';
+import 'dart:isolate';
 
+main (args, SendPort stdout) {
+
+  stdout.send("This is a test Message from SampleScript");
+
+  //Return exitCode different from 0 for testing
+  return 2;
 }
